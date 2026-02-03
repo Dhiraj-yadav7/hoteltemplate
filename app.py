df093d03fe2f285b854f69f2acb88a14e9e3b2d6
Dhiraj Yadav <dhiraj799177@gmail.com>
2026-02-03 18:54:17 +0530

    website is linking issue is just  ready

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/room')
+def room():
+    return render_template('room.html')
+
+@app.route('/amenities')
+def amenities():
+    return render_template('amenities.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
